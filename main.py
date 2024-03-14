02f79d5c414c6ecec585e9488da1594e30fc8275
Koshanzov <koshanzov@gmail.com>
2024-03-14 15:21:06 +0700

    added main.py with function definition

---
+++ main.py
@@ -0,0 +1,16 @@
+russian_vowels = "аоуыэяёюие"
+english_vowels = "aeiouy"
+
+def count_vowel_letters(s: str) -> int:
+	"""
+	Подсчитывает количество гласных букв в
+	строке на русском и английском языках
+	"""
+	s = s.lower()
+	vowels = russian_vowels + english_vowels
+	count = 0
+	for char in s:
+		if char in vowels:
+			count += 1
+
+	return count
